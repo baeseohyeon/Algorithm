018397b148d9d95a915d56d8286ab79a15e606d2
baeseohyeon <formnarago@naver.com>
2021-11-09 19:01:59 +0900

    가장 가까운 공통 조상

---
+++ src/baekjoon/dfs/test3584.java
@@ -0,0 +1,57 @@
+package baekjoon.dfs;
+
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Integer.*;
+
+public class test3584 {
+    static int n;
+    static int ans;
+    static int start, end;
+    static boolean[] visit;
+    static List<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = parseInt(br.readLine());
+        while (t-- > 0) {
+            n = parseInt(br.readLine());
+            list = new ArrayList[n + 1];
+            visit = new boolean[n + 1];
+            ans = 0;
+            for (int i = 0; i <= n; i++) {
+                list[i] = new ArrayList<>();
+            }
+            for (int i = 0; i < n - 1; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = parseInt(st.nextToken());
+                int b = parseInt(st.nextToken());
+                list[b].add(a);
+            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            start = parseInt(st.nextToken());
+            end = parseInt(st.nextToken());
+            dfs(start);
+            dfs(end);
+            bw.write(ans + "\n");
+        }
+        bw.flush();
+    }
+
+    private static void dfs(int now) {
+        if (visit[now]) {
+            ans = now;
+            return;
+        }
+        visit[now] = true;
+        for (int next : list[now]) {
+            if (!visit[next]) {
+                dfs(next);
+            } else if (visit[next]) {
+                ans = next;
+            }
+        }
+    }
+}
