85daebcff236b3cae738b693b9bef320306f3b33
baeseohyeon <formnarago@naver.com>
2022-01-04 16:01:07 +0900

    정수 삼각형

---
+++ src/baekjoon/dp/test1932.java
@@ -0,0 +1,42 @@
+package baekjoon.dp;
+
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class test1932 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = parseInt(st.nextToken());
+            }
+        }
+        dp[0][0] = arr[0][0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + arr[i][j];
+                } else if (j == n - 1) {
+                    dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dp[n - 1][i]);
+        }
+        System.out.println(ans);
+    }
+
+}
